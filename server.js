0c16302ad8a9470f46d579623d9bea6bfc68fa81
devihall <devis0306@gmail.com>
2022-08-27 12:40:07 -0400

    create query for sql

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'earthmars',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
