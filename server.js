d818e02141ea88873134b6bf090f4f0dcc7bd0d9
devihall <devis0306@gmail.com>
2022-08-29 14:34:19 -0400

    update candidate routes to join with party data

--- server.js
+++ server.js
@@ -25,7 +25,11 @@ const db = mysql.createConnection(
 );
 ///query for list of all candidates
 app.get("/api/candidates", (req, res) => {
-  const sql = `SELECT * FROM candidates`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
 
   db.query(sql, (err, rows) => {
     if (err) {
@@ -41,7 +45,12 @@ app.get("/api/candidates", (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id 
+             WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
