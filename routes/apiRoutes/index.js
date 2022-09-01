3c9a243d7025c9f7e341882d5b495c423c16861d
devihall <devis0306@gmail.com>
2022-09-01 16:44:43 -0400

    added voter routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 router.use(require("./candidateRoutes"));
 router.use(require("./partyRoutes"));
-
+router.use(require("./voterRoutes"));
 
 module.exports = router;
